f22322d15d8248a0a4527aa393d1eabec8afc068
Tseringkhando <candy.khando@gmail.com>
2024-04-20 02:13:21 -0400

    minor fixes

--- js/about.js
+++ js/about.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
         { name: "Bandana Pachabhaiya Magar", image: "images/bandana.jpg", position: "Team Member", def: "Passionate about cultivating green spaces and spreading the joy of gardening."},
         { name: "Kusum Lamichaney", image: "images/kusum.jpg", position: "Team Member", def: "Loves spending time with plants."},
         { name: "Rabina Panta", image: "images/me.jpg", position: "Team Member", def: "Passionate about helping others find their perfect green friend."},
-        { name: "Tsering Khando Lama", image: "images/khando.jpg", position: "Team Member", def: "Loves Garden and Grdening." }
+        { name: "Tsering Khando Lama", image: "images/tsering.jpg", position: "Team Member", def: "Loves Garden and Grdening." }
     ];
 
     const container = document.getElementById('teamContainer');
